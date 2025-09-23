35137702c5f3620b2f64ad9ea953a31cba07bae6
ciraht <tharictalon@gmail.com>
2025-09-22 21:42:52 -0300

    adiantamento para amanhã

---
+++ components/MenuNavigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MenuNavigation = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Menu Navigation</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default MenuNavigation;
