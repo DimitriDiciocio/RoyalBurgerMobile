cc814a1b8babd6b9327302f6811350b9de8dbbbb
ciraht <tharictalon@gmail.com>
2025-10-14 10:14:25 -0300

    margintop

--- components/CarouselImg.jsx
+++ components/CarouselImg.jsx
@@ -131,6 +131,7 @@ export default function CarouselImg() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginTop: 40,
     },
     imageContainer: {
         width: width,
